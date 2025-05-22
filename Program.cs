ced68d3d66dffd3674889862dc5741183326924c
“Andrea <andreaveronicam96@gmail.com>
2025-05-22 19:57:10 -0300

    Calculadora V1

--- Program.cs
+++ Program.cs
@@ -1,26 +1,47 @@
-﻿Console.WriteLine("Ingrese el primer numero");
-string PrimerNumero = Console.ReadLine();
-Console.WriteLine("Ingrese el primer numero");
-string segundoNumero = Console.ReadLine();
-  int a;
-  int b;
-  int aux;
-bool ConvertirAIntPrimerNumero = int.TryParse(PrimerNumero, out a)
-;
-bool ConvertirAIntSegundoNumero = int.TryParse(segundoNumero, out b)
-;
-Console.WriteLine("valor de a: "+a);
-Console.WriteLine("valor de b: " + b);
+﻿int opcion;
+int resultado;
+int otroCalculo = 1;
+int a, b;
 
-if (a > 0 && b > 0)
+
+while (otroCalculo == 1)
 {
-   aux = a;
-   a = b;
-   b = aux;
-Console.WriteLine("valor invertido de A: " +a); 
-Console.WriteLine("valor de invertido de B: " +b); 
+   Console.WriteLine("Ingrese la opcion segun la operacion que desea realizar \n1 -SUMA\n2-RESTA\n3-DIVISION\n4-MULTIPLICACION");
+   string menu = Console.ReadLine();
+   bool ConvertirAIntopcion = int.TryParse(menu, out opcion);
+
+   Console.WriteLine("ingrese el primer numero");
+   string primerNumero = Console.ReadLine();
+   bool ConvertirAIntPrimerNumero = int.TryParse(primerNumero, out a);
+
+   Console.WriteLine("ingrese el segundo numero");
+   string segundoNumero = Console.ReadLine();
+   bool ConvertirAIntSegundoNumero = int.TryParse(segundoNumero, out b);
+
+   switch (opcion)
+   {
+      case 1:
+         resultado = a + b;
+         Console.WriteLine($"{a}+{b} = {resultado}");
+         break;
+      case 2:
+         resultado = a - b;
+         Console.WriteLine($"{a}-{b} = {resultado}");
+         break;
+      case 3:
+         resultado = a / b;
+         Console.WriteLine($"{a}/{b} = {resultado}");
+         break;
+      case 4:
+         resultado = a * b;
+         Console.WriteLine($"{a}*{b} = {resultado}");
+         break;
+   }
+
+
+   Console.WriteLine("\n¿Desea realizar otra operación? \n1 - Sí\n2 - No");
+   string respuesta = Console.ReadLine();
+   bool ConvertirAInt = int.TryParse(respuesta, out otroCalculo);
 }
-else
-{
-   Console.WriteLine("para poder invertir los numeros deben ser mayor a 0");
-}
+
+
