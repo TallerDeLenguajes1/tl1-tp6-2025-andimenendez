cf63e836df82cb358beb0d99da118db6463bc5b9
“Andrea <andreaveronicam96@gmail.com>
2025-05-22 23:55:38 -0300

    agregue maximo y minimo entre dos numeros

--- Program.cs
+++ Program.cs
@@ -23,3 +23,26 @@ else
 {
    Console.WriteLine("numero no valido");
 }
+
+float numero1;
+float numero2;
+Console.WriteLine("Ingrese el primer numero");
+string numero1Ingresado = Console.ReadLine();
+bool ConvertirAintNumero1Ingresado = float.TryParse(numero1Ingresado, out  numero1);
+Console.WriteLine("Ingrese el segundo numero");
+string numero2Ingresado = Console.ReadLine();
+bool ConvertirAintNumero2Ingresado = float.TryParse(numero2Ingresado, out numero2);
+
+   if (ConvertirAintNumero1Ingresado && ConvertirAintNumero2Ingresado)
+   {
+   if (numero1 > numero2)
+   {
+      Console.WriteLine($"\nMáximo:{numero1}");
+      Console.WriteLine($"\nMinimo:{numero2}");
+   }
+   else
+   {
+      Console.WriteLine($"\nMáximo:{numero2}");
+      Console.WriteLine($"\nMinimo:{numero1}");
+   }
+   }
