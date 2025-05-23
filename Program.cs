fa009ba586e3cb1b52762e83e5a485bc40976013
“Andrea <andreaveronicam96@gmail.com>
2025-05-22 21:40:27 -0300

    operaciones a partir de un numero

--- Program.cs
+++ Program.cs
@@ -1,56 +1,25 @@
-﻿int opcion;
-int resultado;
-int otroCalculo = 1;
-int a, b;
-
-
-while (otroCalculo == 1)
+﻿float numero;
+Console.WriteLine("Ingrese un numero");
+string numeroIngresado = Console.ReadLine();
+bool ConvertirAintNumeroIngresado = float.TryParse(numeroIngresado, out  numero);
+if (ConvertirAintNumeroIngresado)
 {
-   Console.WriteLine("Ingrese la opcion segun la operacion que desea realizar \n1-SUMA\n2-RESTA\n3-DIVISION\n4-MULTIPLICACION");
-   string menu = Console.ReadLine();
-   bool ConvertirAIntopcion = int.TryParse(menu, out opcion);
-   if (opcion >= 1 && opcion <= 4)
-   {
-      Console.WriteLine("ingrese el primer numero");
-      string primerNumero = Console.ReadLine();
-      bool ConvertirAIntPrimerNumero = int.TryParse(primerNumero, out a);
-
-      Console.WriteLine("ingrese el segundo numero");
-      string segundoNumero = Console.ReadLine();
-      bool ConvertirAIntSegundoNumero = int.TryParse(segundoNumero, out b);
-
-      switch (opcion)
-      {
-         case 1:
-            resultado = a + b;
-            Console.WriteLine($"{a}+{b} = {resultado}");
-            break;
-         case 2:
-            resultado = a - b;
-            Console.WriteLine($"{a}-{b} = {resultado}");
-            break;
-         case 3:
-            resultado = a / b;
-            Console.WriteLine($"{a}/{b} = {resultado}");
-            break;
-         case 4:
-            resultado = a * b;
-            Console.WriteLine($"{a}*{b} = {resultado}");
-            break;
-      }
-   }
-   else
-   {
-      Console.WriteLine("opcion incorrecta");
-   }
-
-   do
-   {
-      Console.WriteLine("\n Ingrese 1: Si desea realizar otra operacion \n 0:Para salir");
-      string Nuevarespuesta = Console.ReadLine();
-      bool ConvertirNuevaRespuestaAInt = int.TryParse(Nuevarespuesta, out otroCalculo);
-   } while ((otroCalculo != 1) && (otroCalculo != 0));
-
-
+      float absoluto = Math.Abs(numero);
+      float cuadrado = numero * numero;
+      double raiz = Math.Sqrt(numero);
+      double seno = Math.Sin(numero);
+      double coseno = Math.Cos(numero);
+      int parteEntera = (int)Math.Truncate(numero);
+    
+   // Mostrar resultados
+      Console.WriteLine($"Valor absoluto: {absoluto}");
+      Console.WriteLine($"Cuadrado: {cuadrado}");
+      Console.WriteLine($"Raíz cuadrada (de números negativos se muestra 0): {raiz}");
+      Console.WriteLine($"Seno: {seno}");
+      Console.WriteLine($"Coseno: {coseno}");
+      Console.WriteLine($"Parte entera: {parteEntera}");
 }
-
+else
+{
+   Console.WriteLine("numero no valido");
+}
